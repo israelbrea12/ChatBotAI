b184d0b4565969a0a755028fda5450cef785c6d6
israelbrea12 <israelbrea.02@gmail.com>
2025-03-11 13:09:50 +0100

    Initial Commit

---
+++ ChatBotAI/ChatBotAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatBotAIApp.swift
+//  ChatBotAI
+//
+//  Created by Israel Brea Piñero on 11/3/25.
+//
+
+import SwiftUI
+
+@main
+struct ChatBotAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChatBotAITests/ChatBotAITests.swift
@@ -0,0 +1,17 @@
+//
+//  ChatBotAITests.swift
+//  ChatBotAITests
+//
+//  Created by Israel Brea Piñero on 11/3/25.
+//
+
+import Testing
+@testable import ChatBotAI
+
+struct ChatBotAITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
